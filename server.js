c1821d727827d27097e3e2e2ea913cc900e950af
jinivetha <46393387+jinivetha@users.noreply.github.com>
2019-10-09 14:27:45 +0530

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const path=require ('path');
 const express=require('express');
 const app=express();
-app.use(express.static(_dirname + '/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/*',function(req,res){
-    res.sendFile(path.join(_dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 app.listen(process.env.PORT||5000);
 console.log('console listening!')
